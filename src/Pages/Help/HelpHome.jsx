1364052bc0896e7be85e8b4dd66d1ce9925b411b
AlfonsoG-dev <alfgomachi@gmail.com>
2024-12-21 17:51:15 -0500

    feat: Create help modal dialog
    - Add style with scrollable content
    - Add help modal for home page
    - Verify the options with the *rol* of the user

--- src/Pages/Help/HelpHome.jsx
+++ src/Pages/Help/HelpHome.jsx
@@ -4,13 +4,15 @@ import PropTypes from "prop-types"
 
 
 // data
+import crear_usuario from "../../../public/docs/images/home/crear_usuario.png"
+import cambiar_contraseña from "../../../public/docs/images/home/cambiar_contraseña.png"
 import crear_historia from "../../../public/docs/images/home/crear_historia.png"
 import mis_historias from "../../../public/docs/images/home/mis_historias.png"
 import descarga_permiso from "../../../public/docs/images/home/descarga_permiso.png"
 
 
 // style
-import "../../Styles/Modal.css"
+import "../../Styles/HelpModal.css"
 
 export function HelpHome({show, handle_close}) {
     const show_hidden = show ? "modal display-block" : "modal display-none"
@@ -19,29 +21,37 @@ export function HelpHome({show, handle_close}) {
     return(
         <div className={show_hidden}>
             <section className="help-modal-main">
-                <span className="close" onClick={handle_close}>
-                    <GoEyeClosed />
-                </span>
-                <h1>Ayuda página Home</h1>
-                <p>En esta sección encontraras las funcionalidades necesarias para el proceso de atención.</p>
-                <ul className="list">
-                    {
-                        user.rol === "admin" && (
-                            <li>Crear usuario</li>
-                        )
-                    }
-                    <li>Crear historia: con esta opción se realiza el registro del pacinete y se abre historia clínica.</li>
-                    <img src={crear_historia} alt="crear_historia_option"/>
-                    <li>Mis historias: esta opción listas las historias que tu creaste</li>
-                    <img src={mis_historias} alt="listar historias"/>
-                    {
-                        user.rol === "admin" && (
-                            <li>Cambiar contraseña</li>
-                        )
-                    }
-                    <li>Descargar permisos: permite descargar el consentimiento y/o disentimiento</li>
-                    <img src={descarga_permiso} alt="descargar permisos"/>
-                </ul>
+                <div className="content">
+                    <span className="close" onClick={handle_close}>
+                        <GoEyeClosed />
+                    </span>
+                    <h1>Ayuda página Home</h1>
+                    <p>En esta sección encontraras las funcionalidades necesarias para el proceso de atención.</p>
+                    <ul className="list">
+                        {
+                            user.rol === "admin" && (
+                                <>
+                                    <li>Crear usuario</li>
+                                    <img src={crear_usuario} alt="crear usuario"/>
+                                </>
+                            )
+                        }
+                        <li>Crear historia: con esta opción se realiza el registro del pacinete y se abre historia clínica.</li>
+                        <img src={crear_historia} alt="crear_historia_option"/>
+                        <li>Mis historias: esta opción listas las historias que tu creaste</li>
+                        <img src={mis_historias} alt="listar historias"/>
+                        {
+                            user.rol === "admin" && (
+                                <>
+                                    <li>Cambiar contraseña</li>
+                                    <img src={cambiar_contraseña} alt="Cambiar contraseña"/>
+                                </>
+                            )
+                        }
+                        <li>Descargar permisos: permite descargar el consentimiento y/o disentimiento</li>
+                        <img src={descarga_permiso} alt="descargar permisos"/>
+                    </ul>
+                </div>
             </section>
         </div>
     )
