a3eff49b11f09f56d40150133733034f588121c4
AlfonsoG-dev <alfgomachi@gmail.com>
2024-12-26 18:43:14 -0500

    feat: add navigation process help

--- src/Pages/Help/HelpHome.jsx
+++ src/Pages/Help/HelpHome.jsx
@@ -4,6 +4,14 @@ import PropTypes from "prop-types"
 
 
 // data
+
+// nav bar
+import nav_bar from "/docs/images/home/hav_bar.png"
+import otro_nav_bar from "/docs/images/home/otro_nav_bar.png"
+import thema_light from "/docs/images/home/thema_light.png"
+import thema_dark from "/docs/images/home/thema_dark.png"
+
+// home
 import crear_usuario from "/docs/images/home/crear_usuario.png"
 import cambiar_contraseña from "/docs/images/home/cambiar_contraseña.png"
 import crear_historia from "/docs/images/home/crear_historia.png"
@@ -26,8 +34,23 @@ export function HelpHome({show, handle_close}) {
                         <GoEyeClosed />
                     </span>
                     <h1>Ayuda página Home</h1>
-                    <p>En esta sección encontraras las funcionalidades necesarias para el proceso de atención.</p>
+                    <p>En esta sección se encuentran las opciones de navegación y opciones para el proceso de atención.</p>
                     <ul className="list">
+                        <li>En la navegación se presentan las distintas páginas a las que se puede acceder</li>
+                        <ol>
+                            <li>Por ejemplo la página Home</li>
+                            {
+                                user.rol === "admin" ? (<img src={nav_bar}/>) : (<img src={otro_nav_bar}/>)
+                            }
+                            <li>Además también se presenta la opción para modificar el esquema de colores de la página.</li>
+                            <ul>
+                                <li>Tema claro</li>
+                                <img src={thema_light}/>
+                                <li>Tema oscuro</li>
+                                <img src={thema_dark}/>
+                            </ul>
+                        </ol>
+                        <h3>Lo siguiente corresponde a las opciones para el proceso de atención</h3>
                         {
                             user.rol === "admin" && (
                                 <>
